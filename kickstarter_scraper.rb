45e4b73dfa3e241199c56d6e23e1d15368e5d671
Dexter Fitch <fitch.dexter@gmail.com>
2020-09-23 19:01:49 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,11 +4,17 @@ require 'pry'
 def create_project_hash
   raw = File.read('fixtures/kickstarter.html')
   html = Nokogiri::HTML(raw)
-  projects = html.css(".project-card")
+  projects_html = html.css(".project-card")
+  projects = {}
+
+  projects_html.each { |project|
+
+  }
   #title: NANINANI.css('.bbcard_name a').text.strip
   #img: NANINANI.css('.project-thumbnail img').attr('src').value
   #desc: NANINANI.css('.bbcard_blurb').text.strip
   #loc: NANINANI.css('.location-name').text.strip
+  # per: NANINANI.css('.funded strong').text.strip
   binding.pry
 end
 
